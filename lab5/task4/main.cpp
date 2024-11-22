7912ca3bd1a13ee7e1717c8d583c748c14f5c763
Siniiy <gulyaev@DESKTOP-JQO9BF9>
2024-11-23 02:23:25 +0300

    task 3 fix

---
+++ lab5/task4/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cmath>
+
+class complex final { 
+    public:
+        complex(double r=0.0, double i=0.0  ) {
+            real = r;
+            img = i;
+        }
+
+        void info() const {
+            std::cout << real << " " << img << "i" << std::endl;
+        }
+        void add(const complex obj) {
+            real += obj.real;
+            img += obj.img;
+        }
+        void substract(const complex obj) {
+            real -= obj.real;
+            img -= obj.img;
+        }
+        void multiply (complex obj) {
+            double temp = real; // (a + ib) * (c + id) = ac + adi + bci - bd
+            real = real * obj.real - img * obj.img;
+            img = temp * obj.img + img * obj.real; 
+        }
+        void divide (complex obj) {
+            double divider = obj.real * obj.real + obj.img * obj.img;
+
+            if(divider < __DBL_EPSILON__) {
+                throw std::overflow_error("Division by zero");
+            }
+
+            obj.img *= -1;
+
+            this->multiply(obj);
+
+            real /= divider;
+            img /= divider;
+        }
+        double module() const {
+            return std::sqrt(real * real + img * img);
+        }
+        double argument() const {
+            return std::atan(img / real) * 180 / M_PI;
+        }
+    virtual ~complex() = default;
+    private:
+        double img, real;
+};
+
+int main() {
+    complex goal = complex(-4, 2);
+
+    complex goal2 = complex(1, -3);
+
+    complex goal3(27.4, 72.8991);
+
+    goal3.add(goal);
+
+    goal3.info();
+
+
+    try { 
+        goal.divide(goal2); 
+        goal.info(); 
+    } 
+    catch (std::overflow_error const &e) { 
+        std::cout << e.what() << std::endl; 
+    }
+    
+    goal.divide(goal2);
+
+    goal.info();
+
+    goal2.info();
+
+    return 0;
+}
